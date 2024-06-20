403dda89304ac22f1926aa072735f15654cf01af
MiledAmer <midou.slm8@gmail.com>
2024-06-20 15:24:24 +0100

    now the users page get data from db

--- src/components/component/view-users-data-table.tsx
+++ src/components/component/view-users-data-table.tsx
@@ -1,7 +1,16 @@
+import { db } from "@/server/db";
 import { Button } from "../ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
 
-export default function ViewUsersDataTable() {
+export default async function ViewUsersDataTable() {
+  const data = await db.utilisateur.findMany();
   return (
     <div className="border shadow-sm rounded-lg">
       <Table>
@@ -13,60 +22,21 @@ export default function ViewUsersDataTable() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">Apple Inc.</TableCell>
-            <TableCell>100</TableCell>
-            <TableCell className="text-right">
-              <Button variant="outline" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="outline" size="sm" className="text-red-500">
-                Delete
-              </Button>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">Microsoft Corp.</TableCell>
-            <TableCell>50</TableCell>
-            <TableCell>$250.00</TableCell>
-            <TableCell>$12,500.00</TableCell>
-            <TableCell className="text-right">
-              <Button variant="outline" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="outline" size="sm" className="text-red-500">
-                Delete
-              </Button>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">Amazon.com, Inc.</TableCell>
-            <TableCell>75</TableCell>
-            <TableCell>$3,000.00</TableCell>
-            <TableCell>$225,000.00</TableCell>
-            <TableCell className="text-right">
-              <Button variant="outline" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="outline" size="sm" className="text-red-500">
-                Delete
-              </Button>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell className="font-medium">Tesla, Inc.</TableCell>
-            <TableCell>25</TableCell>
-            <TableCell>$800.00</TableCell>
-            <TableCell>$20,000.00</TableCell>
-            <TableCell className="text-right">
-              <Button variant="outline" size="sm" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="outline" size="sm" className="text-red-500">
-                Delete
-              </Button>
-            </TableCell>
-          </TableRow>
+          {data &&
+            data.map((row) => (
+              <TableRow key={row.nom}>
+                <TableCell className="font-medium">{row.nom}</TableCell>
+                <TableCell>{row.role}</TableCell>
+                <TableCell className="text-right">
+                  <Button variant="outline" size="sm" className="mr-2">
+                    Edit
+                  </Button>
+                  <Button variant="outline" size="sm" className="text-red-500">
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </div>
