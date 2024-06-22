9177bcab8e5b0fb71b995b1da8de9173fa1c1cb3
MiledAmer <midou.slm8@gmail.com>
2024-06-22 12:48:05 +0100

    add the products page to the side bar

--- src/components/component/view-data-side-bar.tsx
+++ src/components/component/view-data-side-bar.tsx
@@ -37,6 +37,14 @@ export default function ViewDataSideBar() {
                 Stock
               </Link>
               <Link
+                href="/viewdata/products"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                prefetch={false}
+              >
+                <LineChartIcon className="h-4 w-4" />
+                Products
+              </Link>
+              <Link
                 href="/viewdata/productchain"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                 prefetch={false}
